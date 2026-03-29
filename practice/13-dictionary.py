b7e9fbfca16f4849b34bd9941732ba3b2a199967
Ayan Tuladhar <75155513+ayantuladhar1@users.noreply.github.com>
2026-03-29 10:52:36 -0600

    Create 13-dictionary.py

---
+++ practice/13-dictionary.py
@@ -0,0 +1,14 @@
+# Create the dictionary
+car = {"brand": "Ford", "model": "Mustang", "year": 2024}
+
+# Print the model
+print(car["model"])
+
+# Add a color key
+car["color"] = "red"
+
+# Remove the brand key
+car.pop("brand")
+
+# Print the dictionary
+print(car)
