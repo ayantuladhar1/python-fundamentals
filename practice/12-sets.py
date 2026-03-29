db9386f49dbed1451d5bd06ae6a17db8f6e66822
Ayan Tuladhar <75155513+ayantuladhar1@users.noreply.github.com>
2026-03-29 10:52:14 -0600

    Create 12-sets.py

---
+++ practice/12-sets.py
@@ -0,0 +1,14 @@
+# Create the set
+colors = {"red", "green", "blue"}
+
+# Print the set
+print(colors)
+
+# Add "yellow"
+colors.add("yellow")
+
+# Remove "green"
+colors.discard("green")
+
+# Print the number of items
+print(len(colors))
