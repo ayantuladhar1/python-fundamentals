c96ef1c034696895d6fa49e95fadf04b2a1cdd40
Ayan Tuladhar <75155513+ayantuladhar1@users.noreply.github.com>
2026-03-29 10:56:44 -0600

    Create 24-maths.py

---
+++ practice/24-maths.py
@@ -0,0 +1,11 @@
+# Print the lowest value
+print(min(5, 10))
+
+# Print the highest value
+print(max(5, 10))
+
+# Print the absolute value
+print(abs(-7.25))
+
+# Print 4 to the power of 3
+print(pow(4, 3))
